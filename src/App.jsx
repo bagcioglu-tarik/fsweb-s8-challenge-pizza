ec786a2ba2f03dca27dc38df00d875218f1b1cbf
bagcioglu-tarik <tarikbagcioglu@icloud.com>
2026-09-02 15:34:21 +0200

    Project initialized
    - Removed App.css and index.css, replacing them with Reset.css for a cleaner base style.
    - Updated App.jsx to implement React Router for navigation, adding Order and OrderSuccess components.
    - Created Order.jsx and OrderSuccess.jsx components for handling order-related views.
    - Added components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
+import { BrowserRouter, Switch, Route } from 'react-router-dom/cjs/react-router-dom.min'
+import Order from './Order'
+import OrderSuccess from './OrderSuccess'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
-    </>
-  )
+ return (
+  <BrowserRouter>
+     <Switch>
+       <Route path='/' exact>
+        <div>App</div>
+       </Route>
+       <Route path='/order' ><Order/></Route>
+       <Route path='/orderSuccess' ><OrderSuccess/></Route>
+     </Switch>
+   </BrowserRouter>
+ )
 }
 
 export default App
---
+++ src/OrderSuccess.jsx
@@ -0,0 +1,6 @@
+
+export default function OrderSuccess() {
+    return (
+        <div>Successful</div>
+    )
+}
